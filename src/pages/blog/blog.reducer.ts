1a3210f295a4c352ff93893e5cf5ad40c7e42e4f
Huy.th <nhokpio95@gmail.com>
2023-03-22 21:35:28 +0700

    Edit and Delete

--- src/pages/blog/blog.reducer.ts
+++ src/pages/blog/blog.reducer.ts
@@ -1,16 +1,54 @@
-import { createReducer } from '@reduxjs/toolkit'
+import { createAction, createReducer } from '@reduxjs/toolkit'
 import { Post } from '../../types/blog.type'
 import { initalPostList } from '../../constants/blog'
 
 interface BlogState {
   postList: Post[]
+  editingPost: Post | null
 }
 
 const initalState: BlogState = {
-  postList: initalPostList
+  postList: initalPostList,
+  editingPost: null
 }
-const blogReducer = createReducer(initalState, builder => {
 
+export const addPost = createAction<Post>('blog/addPost')
+export const deletePost = createAction<string>('blog/deletePost')
+export const startEditingPost = createAction<string>('blog/startEditingPost')
+export const cancelEditingPost = createAction('blog/cancelEditingPost')
+export const finishEditingPost = createAction<Post>('blog/finishEditingPost')
+const blogReducer = createReducer(initalState, (builder) => {
+  builder
+    .addCase(addPost, (state, action) => {
+      const post = action.payload
+      state.postList.push(post)
+    })
+    .addCase(deletePost, (state, action) => {
+      const postId = action.payload
+      const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+      if (foundPostIndex !== -1) {
+        state.postList.splice(foundPostIndex, 1)
+      }
+    })
+    .addCase(startEditingPost, (state, action) => {
+      const postId = action.payload
+      const foundPost = state.postList.find((post) => post.id === postId) || null
+      state.editingPost = foundPost
+    })
+    .addCase(cancelEditingPost, (state) => {
+      state.editingPost = null
+    })
+    .addCase(finishEditingPost, (state, action) => {
+      const postId = action.payload.id
+      state.postList.some((post, index) => {
+        if (post.id === postId) {
+          state.postList[index] = action.payload
+          return true
+        }
+        return false
+      })
+      state.editingPost = null
+    })
 })
 
 export default blogReducer
